8180075ed653325e386d5364cd7b219b0ffe9054
xeterguyt <75139409+xeterguyt@users.noreply.github.com>
2025-10-05 19:15:53 +0700

    Update UniversalIntro.lua

--- Roblox scripts/UniversalIntro.lua
+++ Roblox scripts/UniversalIntro.lua
@@ -37,7 +37,7 @@ return function()
     logo.Size = UDim2.new(0, 88, 0, 88)
     logo.AnchorPoint = Vector2.new(0.5, 0)
     logo.Position = UDim2.new(0.5, 0, 0, 16)
-    logo.Image = "rbxassetid://108691300199501"
+    logo.Texture = "http://www.roblox.com/asset/?id=108691300199501"
     logo.BackgroundTransparency = 1
     logo.Parent = panel
     logo.ZIndex = logo.Parent.ZIndex+1
